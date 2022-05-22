85b86291fd02410d1f5573ed9b3eed987e0b8d59
Peak <peak1260@gmail.com>
2022-05-22 16:26:23 -0700

    finished up connection between sign in and dashboard

--- my-app/src/sign-in/main.js
+++ my-app/src/sign-in/main.js
@@ -1,8 +1,12 @@
-console.log('Hello World');
-
 const serverUrl = "https://hr43a6esr4uz.usemoralis.com:2053/server";
 const appId = "is22QzmZcFDkWnf2YAOXqLsoSdt62ODzPGeLTUv4";
 Moralis.start({ serverUrl, appId });
+
+let homepage = "http://127.0.0.1:5501/my-app/src/sign-in/index.html";
+if(Moralis.User.current() == null && window.location.href != homepage){
+    document.querySelector('body').style.display = 'none';
+    window.location.href = "index.html";
+}
 
 login = async ()=>{
     await Moralis.authenticate().then(async function (user) {
@@ -14,4 +18,22 @@ login = async ()=>{
     })
 }
 
-document.querySelector("#btn-login").onclick = login;
+logout = async ()=>{
+    await Moralis.User.logOut();
+        console.log('Logged Out');
+        window.location.href = "index.html";
+}
+
+if(document.querySelector("#btn-login") != null){
+    document.querySelector("#btn-login").onclick = login;
+}
+if(document.querySelector("#btn-logout") != null){
+    document.querySelector("#btn-logout").onclick = logout;
+}
+
+//get-balances-link
+//get-transactions-link
+//get-market-link
+//get-NFT-link
+
+

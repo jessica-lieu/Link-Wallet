d997403b17a8292f6c081217172c0e2ea1accfda
ahjsuy <92120716+ahjsuy@users.noreply.github.com>
2022-05-23 19:18:38 -0700

    exchange rates???
    pls

--- my-app/src/sign-in/markettrends.js
+++ my-app/src/sign-in/markettrends.js
@@ -1,54 +1,54 @@
-/* globals Chart:false, feather:false */
+import React, { useState, useEffect} from 'react';
+import axois from 'axios'
+import './App.css';
+import Coin from './coin';
 
-(function () {
-    'use strict'
-  
-    feather.replace({ 'aria-hidden': 'true' })
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        },
-        legend: {
-          display: false
-        }
-      }
-    })
-  })()
+function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axois.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+    }).catch(error=>alert('Error'))
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    )
+
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text"> Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input"
+          onChange={handleChange}/>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return<Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+          />;
+      })}
+    </div>
+
+  );
+}
+
+export default App;
   

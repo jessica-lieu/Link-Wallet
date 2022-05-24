4a040be23999bd5d7d4c4059f2379677054b8efa
Peak <peak1260@gmail.com>
2022-05-23 23:54:45 -0700

    Added transaction table

--- my-app/src/sign-in/main.js
+++ my-app/src/sign-in/main.js
@@ -3,12 +3,12 @@ const appId = "is22QzmZcFDkWnf2YAOXqLsoSdt62ODzPGeLTUv4";
 Moralis.start({ serverUrl, appId });
 
 let homepage = "http://127.0.0.1:5500/index.html";
-if(Moralis.User.current() == null && window.location.href != homepage){
+if (Moralis.User.current() == null && window.location.href != homepage) {
     document.querySelector('body').style.display = 'none';
     window.location.href = "index.html";
 }
 
-login = async ()=>{
+login = async () => {
     await Moralis.authenticate().then(async function (user) {
         console.log('Logged In');
         user.set("name", document.getElementById('user-username').value);
@@ -18,10 +18,10 @@ login = async ()=>{
     })
 }
 
-logout = async ()=>{
+logout = async () => {
     await Moralis.User.logOut();
-        console.log('Logged Out');
-        window.location.href = "index.html";
+    console.log('Logged Out');
+    window.location.href = "index.html";
 }
 
 getBalances = async () => {
@@ -35,9 +35,51 @@ getTransactions = async () => {
         address: "0xcb47540de6771f9a8fAc460F6362d5591897D3ea",
         order: "desc",
         from_block: "0",
-      };
-      const transactions = await Moralis.Web3API.account.getTransactions(options);
-      console.log(transactions);
+    };
+    const transactions = await Moralis.Web3API.account.getTransactions(options);
+    console.log(transactions);
+
+    if (transactions.total > 0) {
+        let table = `
+            <table class="table">
+            <thead>
+            <tr>
+            <th scope="col">Transaction</th>
+            <th scope="col">Block Number</th>
+            <th scope="col">Age</th>
+            <th scope="col">Type</th>
+            <th scope="col">Fee</th>
+            <th scope="col">Value</th>
+                </tr>
+            </thead>
+            <tbody id="theTransactions">
+            </tbody>
+            </table>
+            `;
+        document.querySelector("#tableOfTransactions").innerHTML = table;
+
+        transactions.result.forEach(t => {
+            let content = `
+            <tr>
+                <td><a href='https://rinkeby.etherscan.io/tx/${t.hash
+                }' target="_blank" rel="noopener noreferrer">${t.hash}</a></td>
+                <td><a href='https://rinkeby.etherscan.io/block/${t.block_number
+                }' target="_blank" rel="noopener noreferrer">${t.block_number
+                }</a></td>
+                <td>${millisecondsToTime(
+                    Date.parse(new Date()) - Date.parse(t.block_timestamp)
+                )}</td>
+                <td>${t.from_address == Moralis.User.current().get("ethAddress")
+                    ? "Outgoing"
+                    : "Incoming"
+                }</td>
+                <td>${((t.gas * t.gas_price) / 1e18).toFixed(5)} ETH</td>
+                <td>${(t.value / 1e18).toFixed(5)} ETH</td>
+            </tr>
+            `;
+            theTransactions.innerHTML += content;
+        })
+    }
 }
 
 getMarket = async () => {
@@ -48,23 +90,21 @@ getNFT = async () => {
     console.log("get NFT clicked")
 }
 
-if(document.querySelector("#btn-login") != null){
+if (document.querySelector("#btn-login") != null) {
     document.querySelector("#btn-login").onclick = login;
 }
-if(document.querySelector("#btn-logout") != null){
+if (document.querySelector("#btn-logout") != null) {
     document.querySelector("#btn-logout").onclick = logout;
 }
-if(document.querySelector("#get-balances-link") != null){
+if (document.querySelector("#get-balances-link") != null) {
     document.querySelector("#get-balances-link").onclick = getBalances;
 }
-if(document.querySelector("#get-transactions-link") != null){
+if (document.querySelector("#get-transactions-link") != null) {
     document.querySelector("#get-transactions-link").onclick = getTransactions;
 }
-if(document.querySelector("#get-market-link") != null){
+if (document.querySelector("#get-market-link") != null) {
     document.querySelector("#get-market-link").onclick = getMarket;
 }
-if(document.querySelector("#get-NFT-link") != null){
+if (document.querySelector("#get-NFT-link") != null) {
     document.querySelector("#get-NFT-link").onclick = getNFT;
 }
-
-
